f5ac878c152dedef3025d3c1520b3f6681c59bde
hug0prevoteau <62139503+hug0prevoteau@users.noreply.github.com>
2021-01-08 14:21:55 +0100

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MapContainer, TileLayer, Polyline, CircleMarker, Popup, GeoJSON } from "react-leaflet";
+import stData from "./data/exemple_json.json";
+import linesData from "./data/exemple_json_lines.json";
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'react-bootstrap';
+import Select from 'react-select';
+import { yearOptions, communitiesOptions } from './data/color';
+import metropole from "./data/metropole.json";
+import { Line } from 'react-chartjs-2';
 
-function App() {
+import "./App.css";
+
+function Option(props: OptionProps) {
+  const {
+    children,
+    className,
+    cx,
+    getStyles,
+    isDisabled,
+    isFocused,
+    isSelected,
+    innerRef,
+    innerProps,
+  } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      ref={innerRef}
+      css={getStyles('option', props)}
+      className={cx(
+        {
+          option: true,
+          'option--is-disabled': isDisabled,
+          'option--is-focused': isFocused,
+          'option--is-selected': isSelected,
+        },
+        className
+      )}
+      {...innerProps}
+    >
+      {children}
     </div>
   );
 }
+
+const style = {
+  fillColor: '#F28F3B',
+  weight: 2,
+  opacity: 1,
+  color: 'white',
+  dashArray: '3',
+  fillOpacity: 0.5
+}
+
+
+function App() {
+
+  return (
+    
+    <div>
+      
+      
+
+
+
+
+    <div className = 'blockcentering'>
+      <div className = 'dropdown'>
+        <div>
+      <h1 className='title_site'>Remoteness Influence Factor</h1>
+        </div>
+        <Select className = 'd-inline-block mt-4 col-md-2 col-offset-4'
+        closeMenuOnSelect={true}
+        components={{ Option }}
+        styles={{
+          option: base => ({
+            ...base,
+            border: `1px dotted ${yearOptions[1]}`,
+            height: '100%',
+            width: 300,
+            flex: 1,
+            position: 'relative'
+          }),
+        }}
+        defaultValue={yearOptions[0]}
+        options={yearOptions}
+      />
+      <Select className = 'd-inline-block mt-4 col-md-2 col-offset-4'
+        closeMenuOnSelect={true}
+        components={{ Option }}
+        styles={{
+          option: base => ({
+            ...base,
+            border: `1px dotted ${communitiesOptions[1]}`,
+            height: '100%',
+            width: 300,
+            flex: 1
+          }),
+        }}
+        defaultValue={communitiesOptions[0]}
+        options={communitiesOptions}
+      />
+      </div>
+      <div className = 'button_gap'>
+      <Button variant="dark">Maps</Button>{' '}
+      <Button variant="dark">Stats</Button>{' '}
+      </div>
+      
+      </div>
+    <div className = 'playgroundPreview_1mpV map_lower'>
+    <MapContainer center={[46.71111,1.7191]} zoom={6}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+       <GeoJSON
+          data={metropole}
+          style={() => ({
+            color: '#4a83ec',
+            weight: 0.5,
+            fillColor: "#1a1d62",
+            fillOpacity: 0.3,
+          })}
+        />
+
+
+
+{linesData.map(line => (
+        <Polyline positions={[[
+          line.sourcelat,
+          line.sourcelong],
+          [line.destlat,
+          line.destlong]]} />
+
+        
+        /*<Marker
+          key={st.name}
+          position={[
+            st.latitude,
+            st.longitude
+          ]}
+        />*/
+      ))}
+
+
+
+      {stData.map(st => (
+        /*<CircleMarker 
+        center={[
+          st.latitude,
+          st.longitude]}
+        
+        />*/
+        <CircleMarker center={[
+          st.latitude,
+          st.longitude]} radius={3}>
+          <Popup><b>{st.name}</b> <br /> Contribution to RIF: {st.Contribution_RIF} <br /> Betweenness Centrality: {st.BCn} </Popup>
+        </CircleMarker>
+        
+        /*<Marker
+          key={st.name}
+          position={[
+            st.latitude,
+            st.longitude
+          ]}
+        />*/
+      ))}
+
+      
+
+        
+        
+
+    </MapContainer>
+    </div>
+    
+    </div>
+  )
+  }
+
+
 
 export default App;
---
+++ src/data/color.js
@@ -0,0 +1,26 @@
+export const colourOptions = [
+    { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
+    { value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
+    { value: 'purple', label: 'Purple', color: '#5243AA' },
+    { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
+    { value: 'orange', label: 'Orange', color: '#FF8B00' },
+    { value: 'yellow', label: 'Yellow', color: '#FFC400' },
+    { value: 'green', label: 'Green', color: '#36B37E' },
+    { value: 'forest', label: 'Forest', color: '#00875A' },
+    { value: 'slate', label: 'Slate', color: '#253858' },
+    { value: 'silver', label: 'Silver', color: '#666666' },];
+
+export const yearOptions = [
+    { value: '2013', label: '2013'},
+    { value: '2014', label: '2014'},
+    { value: '2015', label: '2015'},
+    { value: '2016', label: '2016'},
+    { value: '2017', label: '2017'},
+    { value: '2018', label: '2018'}];
+
+export const communitiesOptions = [
+    { value: 'france', label: 'France'},
+    { value: 'uk', label: 'United Kingdom'},
+    { value: 'usa', label: 'United States'},
+    { value: 'spa', label: 'Spain'},
+    { value: 'ita', label: 'Italy'}];
